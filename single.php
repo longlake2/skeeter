eb1a48add355532098709162074faf4b9f3e178d
longlake2 <sites@emailhart.net>
2018-02-23 17:37:56 -0500

    change single post to one column

--- single.php
+++ single.php
@@ -9,29 +9,31 @@
 
 get_header(); ?>
 
-	<div class="container">
-		<div class="row">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
-
-				<?php
-				while ( have_posts() ) : the_post();
-
-					get_template_part( 'template-parts/content', get_post_format() );
-
-					the_post_navigation();
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-
-				endwhile; // End of the loop.
-				?>
-
-				</main><!-- #main -->
-			</div><!-- #primary -->
-
+	<div id="primary" class="content-area-full">
+			<main id="main" class="site-main" role="main">
+ 
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', get_post_format() ); ?>
+ 
+				<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-md-8">
+								<?php the_post_navigation();
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) :
+									comments_template();
+								endif; ?>
+ 
+							</div><!--  .col-md-8 -->
+						</div><!--  .row -->
+					</div><!--  .container -->		
+ 
+			<?php endwhile; // End of the loop. ?>
+ 
+			</main><!-- #main -->
+		</div><!-- #primary -->
+ 
 <?php
-get_sidebar();
+
 get_footer();
